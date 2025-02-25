3751ce3430368757a77c80f9af6709542905c767
MIGUEL_ANGEL_DOMINGUEZ_LOPEZ <a.22308051281173@cbtis128.edu.mx>
2025-02-25 16:58:37 +0000

    dominguez imagen

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,71 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MiImagen());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class MiImagen extends StatelessWidget {
+  const MiImagen({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            "MIS ZAPATOS",
+            style: TextStyle(
+                fontSize: 30.0,
+                fontStyle: FontStyle.italic,
+                color: Colors.white),
+          ),
+          backgroundColor: Color(0xffff655a),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'Miguel Angel Dominguez Lopez 22308051281173 \n',
+                style: TextStyle(
+                    fontSize: 18.0,
+                    fontStyle: FontStyle.italic,
+                    color: Colors.black),
+              ),
+              Image.network(
+                "https://raw.githubusercontent.com/Miguel-Dmz/imagenes-para-app-flutter-6-J/refs/heads/main/air%20max%2097.png",
+                width: 200,
+                height: 200,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const Text(
+                'Tenis Blancos en github',
+                style: TextStyle(
+                    fontSize: 16.0,
+                    fontStyle: FontStyle.italic,
+                    color: Colors.black),
+              ),
+              Image.network(
+                "https://raw.githubusercontent.com/Miguel-Dmz/imagenes-para-app-flutter-6-J/refs/heads/main/air%20mac%20200.png",
+                height: 200, // Ajusta la altura según tus necesidades
+                width: 200, // Ajusta el ancho según tus necesidades
+                fit: BoxFit.cover,
+              ), // Ajusta el modo de ajuste según tus necesidades);
+              const SizedBox(
+                height: 20,
+              ),
+              const Text(
+                'Tenis Blancos con rojo en github',
+                style: TextStyle(
+                    fontSize: 16.0,
+                    fontStyle: FontStyle.italic,
+                    color: Colors.black),
+              ),
+            ],
+          ),
         ),
       ),
-    );
-  }
-}
+    ); // material App
+  } // widgets
+} // clase mi imagen
